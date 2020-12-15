434a68fdfdb2e3bcfcbf76391506369bf62b46a8
B. Drawe <brycerdrawe.b@gmail.com>
2020-12-14 22:43:00 -0700

    fixed manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,27 +2,31 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-  "public/index.html",
-  "public/css/styles.css",
-  "public/js/index.js",
-  "public/js/idb.js",
-  "/icons/icon-72x72.png",
-  "/icons/icon-96x96.png",
-  "/icons/icon-128x128.png",
-  "/icons/icon-152x152.png",
-  "/icons/icon-192x192.png",
-  "/icons/icon-384x384.png",
-  "/icons/icon-512x512.png"
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js",
 ];
 
 // Respond with cached resources
-self.addEventListener('fetch', function(event) {
-    // checks the fetch request. if the request matches a previous request, then return the cache
-    //work already
-    event.respondWith(caches.match(event.request).then(function (request) {
-       return request || fetch(event.request);
-    }));
- });
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  })
 
 // Cache resources
 self.addEventListener('install', function (event) {
@@ -42,7 +46,7 @@ self.addEventListener('install', function (event) {
        cacheKeepList.push(CACHE_NAME);
  
        return Promise.all(keyList.map(function (key, i) {
-          if (cacheKeeplist.indexOf(key) === -1) {
+          if (cacheKeepList.indexOf(key) === -1) {
              console.log('deleting cache : ' + keyList[i]);
              return caches.delete(keyList[i]);
           }
